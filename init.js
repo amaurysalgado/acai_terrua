791d1a25152da92cca96e393d5edfa2a9c6bdfbb
Amaury Salgado <amaurysalgado@gmail.com>
2016-10-29 11:28:05 -0200

    customize header and images

--- init.js
+++ init.js
@@ -5,9 +5,10 @@ var app = express();
 
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('./views/index');
+  res.render('index');
 });
 
 app.listen(3000, function(){
--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -6,9 +6,9 @@ document.getElementById("page-top").onscroll = function() {
 function change_logos(){
   if (window.pageYOffset > 100){
     $("img.img_logo").hide();
-    $("img.img_logo_white").show();
+    $("img.img_logo_small").show();
   }else if (window.pageYOffset < 100){
     $("img.img_logo").show();
-    $("img.img_logo_white").hide();
+    $("img.img_logo_small").hide();
   }
 }
