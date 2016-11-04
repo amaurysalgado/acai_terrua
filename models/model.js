10dbba7c610c516a5f9575c77ed8d3ee1ea7f2f9
Amaury Salgado <amaurysalgado@gmail.com>
2016-11-03 22:20:16 -0200

    add functions update and delete

--- models/model.js
+++ models/model.js
@@ -6,11 +6,12 @@ class Model {
     this.table_name = table_name;
     this.fields = [];
     var self = this;
-    var query = "SELECT * FROM "+ this.table_name+" limit 1; ";
+    var query = "SELECT * FROM "+ this.table_name +" limit 1; ";
     this._execute_query(query, function(err, rows, fields){
       var i = 0;
       var length = fields.length;
       for (i; i < length; i++){
+        // save the columns of the table
         self.fields.push(fields[i].name)
       }
     });
@@ -18,6 +19,7 @@ class Model {
   }
 
   _execute_query(query,callback){
+    console.log("SQL - "+query);
     db.query(query, function(err, rows, fields){
       if(err) throw err;
 
@@ -36,7 +38,6 @@ class Model {
   get(id, callback){
     var query = 'SELECT * FROM '+this.table_name+' WHERE id = '+db.escape(id)+' limit 1;';
     this._execute_query(query, function (err, data, fields) {
-      console.log(query);
       if (data.length > 0){
         callback(err, data[0],fields);
       }else{
@@ -47,9 +48,7 @@ class Model {
 
   where(column, value, callback){
     var query = 'SELECT * FROM '+this.table_name+' '+db.escape(column)+' = '+db.escape(value)+';';
-
     this._execute_query(query, function(err, data, fields){
-      console.log(query);
       if (data.length > 0){
         callback(err, data[0],fields);
       }else{
@@ -59,11 +58,10 @@ class Model {
   };
 
   insert(data, callback){
+    delete data['id'];
     var fields_to_set = [];
     var key = "";
-    var fields = this.getColumns();
     for (key in data){
-      // console.log(this.fields.contains("name"));
       if (this.fields.includes(key)){
         fields_to_set.push(key+" = "+ db.escape(data[key]));
       }
@@ -73,14 +71,43 @@ class Model {
       var query = "INSERT INTO "+this.table_name+" SET ";
       fields_to_set = fields_to_set.join(", ");
       query = query+fields_to_set;
-      console.log(query);
       this._execute_query(query, callback);
     }else {
       callback(false, [])
     }
+  };
 
-  }
+  update(id, data,callback){
+    delete data["id"];
+    var fields_to_set = [];
+    var key = "";
+    for (key in data){
+      if (this.fields.includes(key)){
+        fields_to_set.push(key+" = "+ db.escape(data[key]));
+      }
+    }
 
+    if(fields_to_set.length > 0){
+      var query = "UPDATE "+this.table_name+" SET ";
+      fields_to_set = fields_to_set.join(", ");
+      query = query+fields_to_set;
+      query = query+" WHERE id = "+db.escape(id);
+      this._execute_query(query, callback);
+    }else {
+      callback(false, [])
+    }
+  };
+
+  delete_by_id(id, callback){
+    var query = "DELETE FROM "+this.table_name+" WHERE ID = " + db.escape(id)+";"
+    this._execute_query(query, callback);
+  };
+
+  // @condition - should be escaped before call this function
+  delete_by_condition(condition, callback){
+    var query = "DELETE FROM "+this.table_name+" WHERE " + condition +";"
+    this._execute_query(query, callback);
+  };
 }
 
 module.exports = Model;
