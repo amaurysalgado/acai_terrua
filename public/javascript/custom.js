90fa82c272618fbcf1d1715ba7c73a11019a56ad
Amaury Salgado <amaurysalgado@gmail.com>
2016-11-09 21:03:07 -0200

    fix layout

--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -10,18 +10,3 @@ function change_logos(){
     $("img.img_logo_small").hide();
   }
 }
-
-$("a.modal").on('click',function(){
-  this.style.display = "block";
-});
-
-$(".modal span.close").on('click',function(){
-  modal.style.display = "none";
-});
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
